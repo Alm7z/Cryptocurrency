28ab1065130c81f78afd24945c5abf08b6691f08
Drozdov Anton <Drozdov.anton11@gmail.com>
2017-12-11 18:11:23 +0300

    added calc

---
+++ CryptoCurrency/app/src/main/java/tony/itis/kpfu/ru/cryptocurrency/Calc/CalcUtils.java
@@ -0,0 +1,12 @@
+package tony.itis.kpfu.ru.cryptocurrency.Calc;
+
+import android.widget.TextView;
+
+/**
+ * Created by Tony on 12/11/2017.
+ */
+
+public class CalcUtils {
+
+
+}
---
+++ CryptoCurrency/app/src/main/java/tony/itis/kpfu/ru/cryptocurrency/Calc/CalcView.java
@@ -0,0 +1,30 @@
+package tony.itis.kpfu.ru.cryptocurrency.Calc;
+
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by Tony on 12/11/2017.
+ */
+
+public class CalcView {
+    private TextView currentCoin;
+    private TextView currentWallet;
+
+    private Button Btn0;
+    private Button Btn1;
+    private Button Btn2;
+    private Button Btn3;
+    private Button Btn4;
+    private Button Btn5;
+    private Button Btn6;
+    private Button Btn7;
+    private Button Btn8;
+    private Button Btn9;
+
+    private Button del;
+    private Button dot;
+
+
+
+}
