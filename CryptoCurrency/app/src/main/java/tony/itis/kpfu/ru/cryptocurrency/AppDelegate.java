f279a78316c8ed24b2f5c517c695a37024a2f257
Almaz <Alm5z@mail.ru>
2017-12-11 17:26:16 +0300

    ok

---
+++ CryptoCurrency/app/src/main/java/tony/itis/kpfu/ru/cryptocurrency/AppDelegate.java
@@ -0,0 +1,8 @@
+package tony.itis.kpfu.ru.cryptocurrency;
+
+/**
+ * Created by al on 11.12.17.
+ */
+
+public class AppDelegate {
+}
---
+++ CryptoCurrency/app/src/main/java/tony/itis/kpfu/ru/cryptocurrency/network/InterfaceForNetwork.java
@@ -0,0 +1,8 @@
+package tony.itis.kpfu.ru.cryptocurrency.network;
+
+/**
+ * Created by al on 11.12.17.
+ */
+
+public interface Interface1 {
+}
